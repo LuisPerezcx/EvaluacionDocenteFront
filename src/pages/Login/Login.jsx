6b1af5bcb92f98dd6d01a937002baca3747e445f
LuisPerezcx <luisperez90377@gmail.com>
2025-02-05 14:04:33 -0600

    estructura

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  FooterComponent  from '../../components/FooterComponent';
+import  LoginForm  from './components/LoginForm';
+
+export default function login() {
+  return (
+    <>
+    <LoginForm/>
+    <FooterComponent/>
+    </>
+  )
+}
---
+++ src/pages/Login/components/LoginForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <h1>
+        aqui va el form del login
+    </h1>
+  );
+}
+
+export default App;
